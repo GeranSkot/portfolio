0cea8638380513339fa689d2e34e48ad5068274c
Aleksey Olshevsky <GeranSkot@gmail.com>
2021-02-21 23:21:53 +0300

    spookyScarySkeleton

---
+++ src/components/FaqSection.js
@@ -0,0 +1,43 @@
+import React from 'react';
+
+const FaqSection = () => {
+    return(
+        <div className="faq">
+            <h2>Any questions <span>FAQ</span></h2>
+            <div className="question">
+                <h4>How do i start ?</h4>
+                <div className="answer">
+                    <p>Lorem ipsum dolor sit amet. Lorem ipsum dolor sit amet,
+                        consectetur adipisicing elit. Dicta, eos?
+                    </p>
+                </div>
+            </div>
+            <div className="question">
+                <h4>Daily schedule</h4>
+                <div className="answer">
+                    <p>Lorem ipsum dolor sit amet. Lorem ipsum dolor sit amet,
+                        consectetur adipisicing elit. Dicta, eos?
+                    </p>
+                </div>
+            </div>
+            <div className="question">
+                <h4>Different payment methods</h4>
+                <div className="answer">
+                    <p>Lorem ipsum dolor sit amet. Lorem ipsum dolor sit amet,
+                        consectetur adipisicing elit. Dicta, eos?
+                    </p>
+                </div>
+            </div>
+            <div className="question">
+                <h4>What product do you offer</h4>
+                <div className="answer">
+                    <p>Lorem ipsum dolor sit amet. Lorem ipsum dolor sit amet,
+                        consectetur adipisicing elit. Dicta, eos?
+                    </p>
+                </div>
+            </div>
+        </div>
+    )
+}
+
+export default FaqSection;
